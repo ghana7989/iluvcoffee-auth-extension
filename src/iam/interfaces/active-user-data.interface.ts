48c14f210a2f3c3da469375d1f27647558bad00d
Pavan Chindukuri <chindukuripavan@gmail.com>
2024-08-18 15:46:59 +0530

    implementing intereface for user attached to the request object

---
+++ src/iam/interfaces/active-user-data.interface.ts
@@ -0,0 +1,11 @@
+export interface ActiveUserData {
+  /**
+   * the subject of the token, the value of this property is user ID
+   */
+  sub: string;
+
+  /**
+   * the email of the user
+   */
+  email: string;
+}
